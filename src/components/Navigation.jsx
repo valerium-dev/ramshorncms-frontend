23aeaca3b33ce50ddadbf22837b22f1dbd974742
valerium-dev <admin@valerium.dev>
2021-02-10 17:30:54 -0600

    Updated page titles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ function Navigation(props) {
             <nav class="navbar navbar-expand navbar-dark bg-dark">
                 <div class="container">
                     <Link class="navbar-brand" to="/">
-                        Ramshorn Scholars Program
+                        RSP Coach Management System
                     </Link>
 
                     <div>
