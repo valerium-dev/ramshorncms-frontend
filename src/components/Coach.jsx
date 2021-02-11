b99037668a0f794d05f3f924b2444d1f1a57025d
valerium-dev <admin@valerium.dev>
2021-02-10 23:05:52 -0600

    Added Coach component

---
+++ src/components/Coach.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Coach(){
+    return (
+      <div className="coach">
+        <div class={`container`}>
+            <h1>
+
+            </h1>
+        </div>
+      </div>
+    );
+}
+
+export default Coach;
