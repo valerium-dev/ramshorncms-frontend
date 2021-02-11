f16945d425d6fa521163716335f92ba69a938f4f
valerium-dev <admin@valerium.dev>
2021-02-10 19:59:32 -0600

    Got table component working with API

--- src/components/Coaches.jsx
+++ src/components/Coaches.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { TableWithBrowserPagination, Column } from "react-rainbow-components";
 
 function Coaches() {
+    const [coachData, setCoachData] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://manager-prod.herokuapp.com/coaches`, {method: "GET"})
+            .then(res => res.json())
+            .then(response => {
+                setCoachData(response)
+            })
+            .catch(error => console.log(error));
+    }, []);
+
     return (
         <div className="coaches">
-            <div class="container">
-                <div class="row align-items-center my-5">
-                    <div class="col-lg-7">
-                        <img class="img-fluid rounded mb-4 mb-lg-0"
-                            src="http://placehold.it/900x400"
-                            alt=""/>
-                    </div>
-                    <div class="col-lg-5">
-                        <h1 class="font-weight-light">Coaches</h1>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting
-                            industry. Lorem Ipsum has been the industry's standard dummy text
-                            ever since the 1500s, when an unknown printer took a galley of
-                            type and scrambled it to make a type specimen book.
-                        </p>
-                    </div>
-                </div>
+            <div class="container my-3">
+                <h1>{coachData.length} Coaches</h1>
+                <TableWithBrowserPagination pageSize={30} data={coachData} keyField="id">
+                    <Column header={`First Name`} field={`first_name`}/>
+                    <Column header={`Last Name`} field={`last_name`}/>
+                    <Column header={`EID`} field={`eid`}/>
+                    <Column header={`Email`} field={`email`}/>
+                </TableWithBrowserPagination>
             </div>
         </div>
     );
