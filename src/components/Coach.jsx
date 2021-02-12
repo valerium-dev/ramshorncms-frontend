d0fad5b58aa762e2df173e06f5bc66536a519b02
valerium-dev <admin@valerium.dev>
2021-02-12 15:31:44 -0600

    Mostly completed Coach component UI and data connect

--- src/components/Coach.jsx
+++ src/components/Coach.jsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {Column, Spinner, Table} from 'react-rainbow-components';
 
-function Coach(){
+function Coach(props){
     // TODO: Add editing state
-    // TODO: Add read-only state
-    return (
-      <div className="coach">
-        <div class={`container`}>
-            <h1>
+    // TODO: Add student data and response data
 
-            </h1>
-        </div>
-      </div>
-    );
+    const [coachData, setCoachData] = useState({});
+    const [didLoad, setDidLoad] = useState(false);
+
+    useEffect(() => {
+        fetch(`https://manager-prod.herokuapp.com/coaches/${props.match.params.id}`, {method: "GET"})
+            .then(res => res.json())
+            .then(response => {
+                setCoachData(response);
+                setDidLoad(true);
+            })
+            .catch(error => console.log(error));
+    }, [props.match.params.id]);
+
+    if (didLoad) {
+        return (
+            <div className="coach">
+                <div className={`container my-3`}>
+                    <h1>{coachData.first_name + " " + coachData.last_name}</h1>
+                    <p>{"EID: " + coachData.eid + " | " + coachData.email}</p>
+                </div>
+                <div className="container my-2">
+                    <div class={`row`}>
+                       <div class="col-lg-6">
+                           <h5>{coachData.responses.length} Responses</h5>
+                           <Table isLoading={!didLoad} data={coachData.responses} keyField="id">
+
+                           </Table>
+                       </div>
+                        <div className="col-lg-6">
+                            <h5>{coachData.students.length} Students</h5>
+                            <Table isLoading={!didLoad} data={coachData.students} keyField="id">
+
+                            </Table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return (<Spinner/>);
+    }
 }
 
 export default Coach;
