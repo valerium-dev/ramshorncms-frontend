1be004cd6471c059c93b8fd7cf35ead8d045f997
valerium-dev <admin@valerium.dev>
2021-02-23 20:33:02 -0600

    Added functionality to Students component

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table, Column, MenuItem } from "react-rainbow-components";
+import { Link } from 'react-router-dom';
+import StudentNameCell from './StudentNameCell';
+import CoachNameCell from "./CoachNameCell";
 
 function Students() {
+    let selectedStudents = [];
+
+    const [studentData, setStudentData] = useState([]);
+    const [didLoad, setDidLoad] = useState(false);
+
+    useEffect(() => {
+        fetch(`https://manager-prod.herokuapp.com/students`, {method: "GET"})
+            .then(res => res.json())
+            .then(response => {
+                const studentData = response.map(item => {
+                    return {
+                        "eid": item.eid,
+                        "id": item.id,
+                        "coach": item.coach
+                    }
+                });
+                setStudentData(studentData);
+                setDidLoad(true);
+            })
+            .catch(error => console.log(error));
+    }, []);
+
+    function updateSelection(selection){
+        selectedStudents = selection;
+        document.getElementById('deleteSelected').hidden = !(selectedStudents.length > 0);
+    }
+
+    function deleteSelection() {
+        // TODO: Interface with API for multi delete
+    }
+
     return (
-        <div className="students">
-            <div class="container">
-                <div class="row align-items-center my-5">
-                    <div class="col-lg-7">
-                        <img class="img-fluid rounded mb-4 mb-lg-0"
-                            src="http://placehold.it/900x400"
-                            alt=""/>
-                    </div>
-                    <div class="col-lg-5">
-                        <h1 class="font-weight-light">Students</h1>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting
-                            industry. Lorem Ipsum has been the industry's standard dummy text
-                            ever since the 1500s, when an unknown printer took a galley of
-                            type and scrambled it to make a type specimen book.
-                        </p>
-                    </div>
+        <div className="coaches">
+            <div class="container my-3">
+                <h1>{studentData.length} Students</h1>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <Link to="/newStudent"><button type="button" className="btn btn-secondary">Add Student</button></Link>
+                    <button id="deleteSelected" type="button" className="btn btn-danger" hidden="true" onClick={deleteSelection()}>Delete Selected</button>
+                </div>
+                <div class="my-2">
+                    <Table isLoading={!didLoad} showCheckboxColumn onRowSelection={selection => updateSelection(selection)} data={studentData} keyField="id">
+                        <Column header={`Name`} field={`id`} component={ StudentNameCell }/>
+                        <Column header={`EID`} field={`eid`}/>
+                        <Column header={`Coach`} field={`coach`} component={ CoachNameCell }/>
+                        <Column type="action">
+                            {/* TODO: Call API for delete and redirect to component for editing data */}
+                            <MenuItem label="Edit" onClick={(event, data) => console.log(`Edit ${data.id}`)} />
+                            <MenuItem label="Delete" onClick={(event, data) => console.log(`Delete ${data.id}`)} />
+                        </Column>
+                    </Table>
                 </div>
             </div>
         </div>
