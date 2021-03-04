4485f58539a72a292f8f6dab536929f807c94e56
valerium-dev <admin@valerium.dev>
2021-03-03 21:47:56 -0600

    Implemented Excel import

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import { useFormik } from 'formik';
 
 function Home() {
-    // TODO: Excel import
 
-    return (
+    const [submitted, setSubmitted] = useState(false);
+
+    const formik = useFormik({
+        initialValues: {
+            excel: ''
+        },
+        onSubmit: values => {
+            const data = new FormData();
+            data.append('excel', values.excel);
+            fetch('https://manager-prod.herokuapp.com/import/excel', {
+                method: 'POST',
+                body: data
+            }).then(response => {
+                setSubmitted(true);
+            });
+        }
+    });
+
+    return (!submitted ?
         <div className="home">
-            <div class="container">
-                <div class="row align-items-center my-5">
-                    <div class="col-lg-7">
-                        <img class="img-fluid rounded mb-4 mb-lg-0"
-                            src="http://placehold.it/900x400"
-                            alt=""/>
-                    </div>
-                    <div class="col-lg-5">
-                        <h1 class="font-weight-light">Home</h1>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting
-                            industry. Lorem Ipsum has been the industry's standard dummy text
-                            ever since the 1500s, when an unknown printer took a galley of
-                            type and scrambled it to make a type specimen book.
-                        </p>
+            <div class="container text-center py-3">
+                <button className="btn btn-success" data-bs-toggle="modal" data-bs-target="#fileUploadModal">
+                    <i className="fas fa-file-excel"/> Import from Excel
+                </button>
+            </div>
+            <div class="modal fade" id="fileUploadModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Import from Excel file</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"/>
+                        </div>
+                        <div class="modal-body">
+                            <p class="text-danger">
+                                Importing from Excel files deletes all data stored in the system.
+                                <strong> Proceed with caution.</strong>
+                            </p>
+                            <form id="fileUpload" onSubmit={formik.handleSubmit}>
+                                <label htmlFor="excel" className="form-label">Select the Excel spreadsheet to import:</label>
+                                <input id="excel"
+                                       class="form-control"
+                                       type="file"
+                                       name="excel"
+                                       onChange={(event)=>{
+                                           formik.setFieldValue("excel", event.target.files[0]);
+                                       }}
+                                       onBlur={formik.handleBlur}/><br/>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <input type="submit" form="fileUpload" class="btn btn-primary"
+                                   data-bs-toggle="modal"
+                                   data-bs-target="#fileUploadModal"
+                                   value="Upload File"/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> : <Redirect to="/coaches"/>
     );
 }
 
